fa080bce6fe8e1e4b617b27f7e85c92835a962e0
Adam Schwalm <adamschwalm@gmail.com>
2015-06-28 22:16:31 -0500

    Add working fiche predictor

--- predict_fiche.py
+++ predict_fiche.py
@@ -1,31 +1,34 @@
 import predict_msvc
 import itertools
 
-# 7584    -> 24
-# 19164   -> 12
-# 25795   -> 19
-# 22125   -> 21
+# 365    -> 5
+# 1216   -> 28
+# 5415   -> 15
+# 16704  -> 0
 
-# 5828
-# 23405
-# 27477
+# 24504  -> 24
+# 11254  -> 22
+# 24698  -> 2
+# 1702   -> 10
 
-values = [24, 12, 19]
+def msvc_lcg_next(state):
+    return predict_msvc.lcg(state, 214013, 2531011, 2**31, 0)
 
-possible = [[] for v in values]
+values = [5, 28, 15, 0, 24, 22]
 
-for i, l in enumerate(possible):
-    for x in range(0, 2**16):
-        if x % 36 == values[i]:
-            l.append(x)
+for i in range(2**16):
+    if i % 36 == values[0]:
+        for j in range(2**16):
+            a = i << 16
+            a |= j
 
-print [len(l) for l in possible]
+            next = msvc_lcg_next(a)
 
-for p1 in possible[0]:
-    for p2 in possible[1]:
-        for p3 in possible[2]:
-            if next(predict_msvc.generate_values([p1, p2, p3], a=214013, c=2531011, m=2**31, masked_bits=16, noexcept=True)):
-                print("Values: ", p1, p2, p3)
-
-# for i in itertools.islice(:
-#     print i
+            for v in values[1:]:
+                if (next >> 16) % 36 == v:
+                    next = msvc_lcg_next(next)
+                else:
+                    break
+            else:
+                print(next >> 16)
+                break
