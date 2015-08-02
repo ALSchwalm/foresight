8dbe88959c71074a01336ae75595eecddd222db4
Adam Schwalm <adamschwalm@gmail.com>
2015-08-01 22:46:27 -0500

    Switch to argparse for better subparsers

--- predict_rng.py
+++ predict_rng.py
@@ -1,23 +1,7 @@
 #!/usr/bin/env python
-"""
-Usage:
-  predict_rng msvc  (rand|rand_r) <count> <output>...
-  predict_rng msvc  (rand|rand_r) <count> --seed=<seed>
-  predict_rng glibc (rand|random|rand_r) <count> <output>...
-  predict_rng glibc (rand|random|rand_r) <count> --seed=<seed>
-  predict_rng php   (linux|windows) rand <count> <output>...
-  predict_rng php   (linux|windows) rand <count> --seed=<seed>
-  predict_rng lcg <multiplier> <increment> <modulus> <bitshift> <count> <output>...
-  predict_rng lcg <multiplier> <increment> <modulus> <bitshift> <count> --seed=<seed>
 
-Description:
-  Given OUTPUTs from a random number generator (glibc's rand_r, PHP rand, etc),
-  predict_rng predicts the next COUNT outputs. Alternately, predict_rng can
-  be passed the SEED, which will be used to produce the first COUNT outputs
-  that would be returned from the random number generator.
-"""
-
-from docopt import docopt
+import argparse
+from textwrap import dedent
 from predrng import lcg, glibc, php
 from predrng.glibc import random, rand_r
 from predrng.php import rand
@@ -29,71 +13,133 @@ def print_from_gen(generator, count):
         print(v)
 
 
-def handle_msvc(args, count, outputs, seed):
-    if args["rand"] or args["rand_r"]:
-        if seed:
-            print_from_gen(lcg.generate_from_seed(seed), count)
+def handle_msvc(args):
+    if args.function in ("rand",):
+        if args.seed:
+            print_from_gen(lcg.generate_from_seed(args.seed), args.count)
         else:
-            print_from_gen(lcg.generate_from_outputs(outputs), count)
+            print("here")
+            print_from_gen(lcg.generate_from_outputs(args.outputs), args.count)
 
 
-def handle_lcg(args, count, outputs, seed):
-    a = int(args["<multiplier>"])
-    c = int(args["<increment>"])
-    m = int(args["<modulus>"])
-    bitshift = int(args["<bitshift>"])
-    if seed:
-        print_from_gen(lcg.generate_from_seed(seed, a, c, m, bitshift), count)
+def handle_lcg(args):
+    if args.seed:
+        print_from_gen(lcg.generate_from_seed(args.seed,
+                                              args.multiplier,
+                                              args.increment,
+                                              args.modulus,
+                                              args.bitshift), args.count)
     else:
-        print_from_gen(lcg.generate_from_outputs(outputs, a, c, m, bitshift), count)
+        print_from_gen(lcg.generate_from_outputs(args.outputs,
+                                                 args.multiplier,
+                                                 args.increment,
+                                                 args.modulus,
+                                                 args.bitshift), args.count)
 
 
-def handle_glibc(args, count, outputs, seed):
-    if args["rand"] or args["random"]:
-        if seed:
-            print_from_gen(glibc.random.generate_from_seed(seed), count)
+def handle_glibc(args):
+    if args.function in ("rand", "random"):
+        if args.seed:
+            print_from_gen(glibc.random.generate_from_seed(args.seed), args.count)
         else:
-            print_from_gen(glibc.random.generate_from_outputs(outputs), count)
-    elif args["rand_r"]:
-        if seed:
-            print_from_gen(glibc.rand_r.generate_from_seed(seed), count)
+            print_from_gen(glibc.random.generate_from_outputs(args.outputs), args.count)
+    elif args.function == "rand_r":
+        if args.seed:
+            print_from_gen(glibc.rand_r.generate_from_seed(args.seed), args.count)
         else:
-            print_from_gen(glibc.rand_r.generate_from_outputs(outputs), count)
+            print_from_gen(glibc.rand_r.generate_from_outputs(args.outputs), args.count)
 
 
-def handle_php(args, count, outputs, seed):
-    if args["windows"]:
-        platform = "windows"
-    elif args["linux"]:
-        platform = "linux"
-
-    if args["rand"]:
-        if seed:
-            print_from_gen(php.rand.generate_from_seed(seed, platform), count)
+def handle_php(args):
+    if args.function == "rand":
+        if args.seed:
+            print_from_gen(php.rand.generate_from_seed(args.seed, args.platform), args.count)
         else:
-            print_from_gen(php.rand.generate_from_outputs(outputs, platform), count)
+            print_from_gen(php.rand.generate_from_outputs(args.outputs, args.platform), args.count)
+
+def add_basic_configuration(parser):
+    parser.add_argument("count", type=int,
+                        help="The number of outputs to predict")
+
+    parser.add_argument("-s", "--seed", type=int,
+                        help="The initial seed")
+    parser.add_argument("outputs", nargs="*", type=int,
+                        help="Outputs from this RNG", metavar="output")
+
+def setup_glibc_parser(sp):
+    sp_glibc = sp.add_parser("glibc", help="Predict outputs from GNU C Library random functions")
+    sp_glibc.add_argument('function', choices=('rand', 'random', 'rand_r'),
+                          help="Source of outputs")
+    add_basic_configuration(sp_glibc)
+
+
+def setup_msvc_parser(sp):
+    sp_msvc = sp.add_parser("msvc", help="Predict outputs from Microsoft Visual C++ random functions")
+    sp_msvc.add_argument('function', choices=('rand',),
+                         help="Source of outputs")
+    add_basic_configuration(sp_msvc)
+
+
+def setup_php_parser(sp):
+    sp_php = sp.add_parser("php", help="Predict outputs from PHP random functions")
+    sp_php.add_argument('platform', choices=('linux', 'windows'),
+                        help="Platform PHP is running on")
+    sp_php.add_argument('function', choices=('rand',),
+                        help="Source of outputs")
+    add_basic_configuration(sp_php)
+
+
+def setup_lcg_parser(sp):
+    sp_lcg = sp.add_parser("lcg", help="Predict outputs from an arbitrary linear congruential generator",
+                           formatter_class=argparse.RawTextHelpFormatter,
+                           description=dedent("""
+    Determine the next value of a linear congruential generator
+    An LCG outputs values with the following recurrence relation:
+
+        state_(n+1) = MULTIPLIER * state_n + INCREMENT (mod MODULUS)
+        value_n = state_n >> BITSHIFT"""))
+
+    sp_lcg.add_argument("multiplier", type=int)
+    sp_lcg.add_argument("increment", type=int)
+    sp_lcg.add_argument("modulus", type=int)
+    sp_lcg.add_argument("bitshift", type=int)
+
+    add_basic_configuration(sp_lcg)
 
 
 def main():
-    args = docopt(__doc__)
+    parser = argparse.ArgumentParser(prog='predict_rng', description="""
+    Given outputs from a random number generator (glibc's rand_r, PHP rand, etc),
+    predict_rng predicts the future outputs. Alternately, predict_rng can
+    be passed the seed, which will be used to produce same values that would
+    be returned from the random number generator.""",
+                                     epilog="See additional help for a command with: predict_rng <command> -h")
 
-    outputs = None
-    if args["<output>"]:
-        outputs = [int(output) for output in args["<output>"]]
+    parser.add_argument("-m", "--modulo", type=int, nargs=1,
+                        help="Output values modulo this number", metavar="MOD")
 
-    seed = None
-    if args["--seed"]:
-        seed = int(args["--seed"])
+    sp = parser.add_subparsers(dest="command")
+    sp.metavar = 'command'
 
-    count = int(args["<count>"])
-    if args["msvc"]:
-        handle_msvc(args, count, outputs, seed)
-    elif args["lcg"]:
-        handle_lcg(args, count, outputs, seed)
-    elif args["glibc"]:
-        handle_glibc(args, count, outputs, seed)
-    elif args["php"]:
-        handle_php(args, count, outputs, seed)
+    # Setup sub parsers
+    setup_glibc_parser(sp)
+    setup_msvc_parser(sp)
+    setup_php_parser(sp)
+    setup_lcg_parser(sp)
+
+    ns = parser.parse_args()
+
+    if vars(ns).get("seed") and vars(ns).get("outputs"):
+        parser.error("Outputs are unnecessary when a seed is given")
+
+    if ns.command == "glibc":
+        handle_glibc(ns)
+    elif ns.command == "msvc":
+        handle_msvc(ns)
+    elif ns.command == "php":
+        handle_php(ns)
+    elif ns.command == "lcg":
+        handle_lcg(ns)
 
 if __name__ == "__main__":
     main()
