b6fcc10fdebef6cd2c7cd312876804e608d5213b
Adam Schwalm <adamschwalm@gmail.com>
2017-05-07 12:34:41 -0500

    Fixed bugs in glibc prediction

--- foresight/glibc/random.py
+++ foresight/glibc/random.py
@@ -15,7 +15,7 @@ __all__ = [
 
 class State(object):
     """ A simple type representing a numeric value, where the least
-    significant bit my be currently unknown.
+    significant bit may be currently unknown.
     """
     def __init__(self, value, lsb):
         self.value = value
@@ -40,14 +40,13 @@ def predict_state(values):
         predicted_value += values[i-31].predicted()
         predicted_value = (predicted_value % 2**32) >> 1
 
-        # Sanity check
-        if (not values[i-3].uncertain) and (not values[i-31].uncertain):
-            assert(value.value == predicted_value)
-
         if value.value == predicted_value:
-            if not values[i-31].uncertain and values[i-31].lsb:
-                values[i-3].lsb = 0
+            if not values[i-31].uncertain:
                 values[i-3].uncertain = False
+                if values[i-31].lsb:
+                    values[i-3].lsb = 0
+                else:
+                    values[i-3].lsb = 1
         elif value.value == predicted_value+1:
             values[i-3].lsb = 1
             values[i-3].uncertain = False
@@ -57,29 +56,36 @@ def predict_state(values):
             # Sanity check
             assert(False)
 
-    for v in values[-38:-4]:
-        if v.uncertain:
-            print("\nWarning: Unable to recover all internal state (not enough outputs).")
-            print("Some predictions my be incorrect.\n")
-            break
+        # Sanity check
+        if (not values[i-3].uncertain) and (not values[i-31].uncertain):
+            predicted_value = values[i-3].predicted()
+            predicted_value += values[i-31].predicted()
+            predicted_value = (predicted_value % 2**32) >> 1
+            assert(value.value == predicted_value)
+
+    #TODO: remove this once I can figure out why the predictions are sometimes
+    # incorrect even when there is no remaining uncertainty (around 500 inputs)
+    print("\nWarning: Some predictions my be incorrect.\n")
     return [v.predicted() for v in values[-38:-4]]
 
 
 def generate_values(state):
-    while(True):
-        predicted_value = state[31]
-        predicted_value += state[3]
-        predicted_value = predicted_value % 2**32
-        state = state[1:] + [predicted_value]
-        yield predicted_value >> 1
+    def inner(state):
+        while True:
+            predicted_value = state[31]
+            predicted_value += state[3]
+            predicted_value = predicted_value % 2**32
+            state = state[1:] + [predicted_value]
+            yield predicted_value >> 1
+    gen = inner(state)
+    for _ in range(4):
+        next(gen)
+    yield from gen
 
 
 def from_outputs(prev_values):
     state = predict_state(prev_values)
-    gen = generate_values(state)
-    for _ in range(4):
-        next(gen)
-    yield from gen
+    yield from generate_values(state)
 
 
 def from_seed(seed):
@@ -88,6 +94,8 @@ def from_seed(seed):
         state.append((16807 * state[i-1]) % (2**31-1))
     state += state[0:3]
     gen = generate_values(state)
-    for _ in range(310):
+
+    # 4 are skipped by generate_values, so skip remaining 306
+    for _ in range(306):
         next(gen)
     yield from gen
